2fcd267f56b7225a5559981792e015e9851e1abb
sujiaheng <648952316@qq.com>
2021-08-04 18:14:41 +0800

    try implement scrool view

--- src/PPT.tsx
+++ src/PPT.tsx
@@ -13,6 +13,8 @@ type PPTWrapperState = {
 }
 
 class PPTWrapper extends React.Component<PPTWrapperProps, PPTWrapperState> {
+    private wrapperRef: HTMLDivElement | null = null;
+    private viewRef: HTMLDivElement | null = null;
 
     constructor(props: PPTWrapperProps) {
         super(props);
@@ -26,12 +28,12 @@ class PPTWrapper extends React.Component<PPTWrapperProps, PPTWrapperState> {
     }
 
     setRef = (ref: HTMLDivElement) => {
-        console.log(this.props);
+        this.viewRef = ref;
         this.props.view.divElement = ref;
         this.props.view.focusScenePath = `${this.props.initScenePath}/${this.getCurrentPathName()}`;
     }
 
-    onClick = () => {
+    nextPage = () => {
         this.setState({ page: this.state.page + 1 });
         this.props.view.focusScenePath = `${this.props.initScenePath}/${this.getCurrentPathName()}`;
     }
@@ -40,11 +42,33 @@ class PPTWrapper extends React.Component<PPTWrapperProps, PPTWrapperState> {
         return this.props.scenes[this.state.page - 1]?.name;
     }
 
+    onWheel = () => {
+        if (this.viewRef) {
+            this.viewRef.style.pointerEvents = "none";
+        }
+    }
+
+    onClick = () => {
+        if (this.viewRef) {
+            this.viewRef.style.pointerEvents = "auto";
+        }
+    }
+
+    setWrapperRef = (ref: HTMLDivElement) => {
+        this.wrapperRef = ref;
+    }
+
     render() {
         return (
             <div
-                ref={this.setRef}
-                style={{ width: "100%", height: "100%" }}>
+                ref={this.setWrapperRef}
+                onWheel={this.onWheel}
+                onClick={this.onClick}
+                style={{ width: "100%", height: "700px" }}>
+                <div
+                    ref={this.setRef}
+                    style={{ width: "100%", height: "100%" }}>
+                </div>
             </div>
         );
     }
@@ -55,13 +79,13 @@ export default {
     options: {
         enableView: true,
         width: 500,
-        height: 1000
+        height: 600
      },
     setup: (context: Context) => {
         context.on("create", () => {
             console.log("create");
             context.setAttributes({ aaaaa: 1 });
-            context.emit("setBoxSize", { width: 400, height: 400 });
+            // context.emit("setBoxSize", { width: 400, height: 400 });
         });
     },
     wrapper: PPTWrapper,
