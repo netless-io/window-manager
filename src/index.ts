4afbd5b3fa0b2c83da334a8156b9b779d7a380c0
sujiaheng <648952316@qq.com>
2021-11-19 15:19:55 +0800

    0.3.0-canary.2

--- src/index.ts
+++ src/index.ts
@@ -170,7 +170,7 @@ export class WindowManager extends InvisiblePlugin<WindowMangerAttributes> {
     public static containerSizeRatio = DEFAULT_CONTAINER_RATIO;
     private static isCreated = false;
 
-    public version = "0.3.0-canary.1";
+    public version = "0.3.0-canary.2";
 
     public appListeners?: AppListeners;
 
