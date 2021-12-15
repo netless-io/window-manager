07822d209d6f5b27ef01b5a006eeae67e06dd827
sujiaheng <648952316@qq.com>
2021-12-15 18:17:03 +0800

    chore: release 0.3.12

--- src/index.ts
+++ src/index.ts
@@ -175,7 +175,7 @@ export class WindowManager extends InvisiblePlugin<WindowMangerAttributes> {
     public static containerSizeRatio = DEFAULT_CONTAINER_RATIO;
     private static isCreated = false;
 
-    public version = "0.3.12-canary.1";
+    public version = "0.3.12";
 
     public appListeners?: AppListeners;
 
