808feae3a17728a794592b8e484495c2c97eaee1
sujiaheng <648952316@qq.com>
2022-01-17 12:33:06 +0800

    chore: release 0.3.20

--- src/index.ts
+++ src/index.ts
@@ -177,7 +177,7 @@ export class WindowManager extends InvisiblePlugin<WindowMangerAttributes> {
     public static containerSizeRatio = DEFAULT_CONTAINER_RATIO;
     private static isCreated = false;
 
-    public version = "0.3.19";
+    public version = "0.3.20";
 
     public appListeners?: AppListeners;
 
