5aa9a38f6e063cdaccc7d7a34f559722235a9af7
sujiaheng <648952316@qq.com>
2022-01-19 11:07:43 +0800

    e2e: fix mainViewScenePath tests

--- e2e/cypress/integration/mainViewScenePath.spec.ts
+++ e2e/cypress/integration/mainViewScenePath.spec.ts
@@ -19,60 +19,64 @@ describe("切换 MainViewScene", () => {
         cy.wait(1000);
     });
 
-    it("set index 0", () => {
+    it("设置 MainViewSceneIndex", () => {
         cy.window().then(async (window: any) => {
             const manager = window.manager as WindowManager;
             const room = window.room;
             expect(room).to.be.a("object");
             expect(manager).to.be.a("object");
 
-            cy.wrap(null).then(() => manager.setMainViewSceneIndex(0))
+            cy.wrap(null).then(() => manager.setMainViewSceneIndex(0));
 
             expect(manager.mainViewSceneIndex).to.be.equal(0);
             expect(manager.mainView.focusScenePath).to.be.equal("/init");
         });
     });
 
-    it("set scenePath update sceneIndex", () => {
+    it("设置 mainViewScenePath 更新 mainViewSceneIndex", () => {
         cy.window().then((window: any) => {
             const manager = window.manager as WindowManager;
             const room = window.room;
             expect(room).to.be.a("object");
             expect(manager).to.be.a("object");
-        
-            manager.setMainViewSceneIndex(1);
+
+            cy.wrap(null).then(() => manager.setMainViewSceneIndex(1));
 
             cy.wait(1000).then(() => {
                 expect(manager.mainViewSceneIndex).to.be.equal(1);
             });
 
-            manager.setMainViewScenePath("/");
+            cy.wrap(null).then(() => manager.setMainViewScenePath("/"));
 
             cy.wait(1000).then(() => {
                 expect(manager.mainViewSceneIndex).to.be.equal(0);
             });
-            
         });
     });
 
-    it("remove scenes update mainView scenePath", () => {
+    it("删除 scenes 更新 sceneIndex", () => {
         cy.window().then(async (window: any) => {
             const manager = window.manager as WindowManager;
             const room = window.room as Room;
             expect(room).to.be.a("object");
             expect(manager).to.be.a("object");
             room.putScenes("/", [{}]);
-            
+
             cy.wrap(null).then(() => manager.setMainViewSceneIndex(1));
 
-            const focusScenePath = manager.mainView.focusScenePath;
-            if (focusScenePath) {
-                room.removeScenes(focusScenePath);
-                expect(manager.mainViewSceneIndex).to.be.equal(0);
-                expect(manager.mainView.focusScenePath).to.be.equal("/init");
-            } else {
-                expect(true).to.be.false;
-            }
+            cy.wait(1000).then(() => {
+                const focusScenePath = manager.mainView.focusScenePath;
+                if (focusScenePath) {
+                    expect(focusScenePath).to.be.not.equal("/init");
+                    cy.wrap(null).then(() => room.removeScenes(focusScenePath));
+                    cy.wait(1000).then(() => {
+                        expect(manager.mainViewSceneIndex).to.be.equal(0);
+                        expect(manager.mainView.focusScenePath).to.be.equal("/init");
+                    });
+                } else {
+                    expect(true).to.be.false;
+                }
+            });
         });
     });
 });
--- e2e/cypress/integration/readonly.spec.ts
+++ e2e/cypress/integration/readonly.spec.ts
@@ -1,15 +1,15 @@
-import { RoomPhase } from 'white-web-sdk';
-import type { Room} from "white-web-sdk";
+import { RoomPhase } from "white-web-sdk";
+import type { Room } from "white-web-sdk";
 
 describe("只读模式加入房间", () => {
     before(() => {
         cy.visit("/?isWritable=false");
         cy.wait(8000);
-    })
+    });
 
     afterEach(() => {
-        cy.wait(1000)
-    })
+        cy.wait(1000);
+    });
 
     it("挂载成功", () => {
         cy.window().then((window: any) => {
@@ -18,9 +18,9 @@ describe("只读模式加入房间", () => {
             expect(room.phase).to.be.equal(RoomPhase.Connected);
             expect(manager).to.be.a("object");
             expect(room.isWritable).to.be.false;
-            cy.get(".netless-whiteboard").should("have.length", 1);      
-        })
-    })
-})
+            cy.get(".netless-whiteboard").should("have.length", 1);
+        });
+    });
+});
 
 export {};
