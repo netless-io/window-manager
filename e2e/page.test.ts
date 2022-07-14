0645977d50717d84d5855c1511cb9394777949eb
sujiaheng <648952316@qq.com>
2022-07-14 13:56:26 +0800

    e2e: add moveCamera & jumpPage test

--- e2e/page.test.ts
+++ e2e/page.test.ts
@@ -3,31 +3,42 @@ import { createRoom, gotoRoom, getWindow, getPageState } from "./helper";
 
 test.describe("页面操作", () => {
     test.beforeEach(async ({ page }) => {
-       const { uuid, token } = await createRoom();
-       await gotoRoom(page, uuid, token);
-    })
+        const { uuid, token } = await createRoom();
+        await gotoRoom(page, uuid, token);
+    });
 
-    test("添加切换删除", async ({ page }) => {
+    test("添加-切换-删除-跳页", async ({ page }) => {
         const handle = await getWindow(page);
-        
+
         await handle.evaluate(async window => {
             await window.manager.addPage();
         });
-        const pageState = await getPageState(handle)
+        const pageState = await getPageState(handle);
         expect(pageState).toMatchObject({ index: 0, length: 2 });
 
         await handle.evaluate(async window => {
-            await window.manager.nextPage()
+            await window.manager.nextPage();
         });
 
-        const pageState2 = await getPageState(handle)
+        const pageState2 = await getPageState(handle);
         expect(pageState2).toMatchObject({ index: 1, length: 2 });
 
         await handle.evaluate(async window => {
-            await window.manager.removePage(1)
+            await window.manager.removePage(1);
         });
 
-        const pageState3 = await getPageState(handle)
+        const pageState3 = await getPageState(handle);
         expect(pageState3).toMatchObject({ index: 0, length: 1 });
-    })
+
+        await handle.evaluate(async window => {
+            await window.manager.addPage();
+            await window.manager.addPage();
+        });
+
+        await handle.evaluate(async window => {
+            await window.manager.jumpPage(2);
+        });
+        const pageState4 = await getPageState(handle);
+        expect(pageState4).toMatchObject({ index: 2, length: 3 });
+    });
 });
--- e2e/view-sync.test.ts
+++ e2e/view-sync.test.ts
@@ -1,3 +1,4 @@
+import type { Page } from "@playwright/test";
 import { test, expect } from "@playwright/test";
 import { gotoRoom, createRoom, createAnotherPage, getWindow } from "./helper";
 
@@ -13,17 +14,8 @@ test.describe("视角同步", () => {
 
     test("主白板", async ({ page, browser }) => {
         await gotoRoom(page, uuid, token);
-        const size = page.viewportSize();
-        if (!size) return;
-        const x = size.width / 2;
-        const y = size.height / 2;
-        await page.mouse.move(x, y);
-        await page.mouse.down();
-        await page.mouse.move(x + 50, y + 50);
-        await page.mouse.up();
-        // 笔画只有画的端跟接收端会有细微的不同,刷新后笔迹才相同
-        await page.reload();
-        await page.waitForTimeout(2000);
+        await drawLine(page);
+
         const buffer = await page
             .locator(".netless-window-manager-main-view .netless-whiteboard")
             .screenshot();
@@ -34,6 +26,8 @@ test.describe("视角同步", () => {
             .locator(".netless-window-manager-main-view .netless-whiteboard")
             .screenshot();
         expect(buffer).toEqual(buffer2);
+        await page.close();
+        await page2.page.close();
     });
 
     test("setContainerSizeRatio", async ({ page }) => {
@@ -52,5 +46,44 @@ test.describe("视角同步", () => {
         });
         const camera2 = await handle.evaluate(w => w.manager.mainView.camera);
         expect(camera).toEqual(camera2);
+        await page.close();
+    });
+
+    test("moveCamera", async ({ page, browser }) => {
+        await gotoRoom(page, uuid, token);
+        let handle = await getWindow(page);
+        await page.waitForTimeout(1000);
+        const camera = await handle.evaluate(w => w.manager.mainView.camera);
+        expect(camera).toBeDefined();
+
+        await drawLine(page);
+
+        const page2 = await createAnotherPage(browser, uuid, token);
+
+        handle = await getWindow(page);
+        await handle.evaluate(async w => {
+            w.manager.moveCamera({ scale: 1.5 });
+        });
+        await page.waitForTimeout(1000);
+        const camera1 = await handle.evaluate(w => w.manager.mainView.camera);
+        const camera2 = await page2.handle.evaluate(w => w.manager.mainView.camera);
+        expect(camera1).toEqual(camera2);
+        await page.close();
+        await page2.page.close();
     });
 });
+
+const drawLine = async (page: Page) => {
+    const size = page.viewportSize();
+    if (!size) return;
+    const x = size.width / 2;
+    const y = size.height / 2;
+    await page.mouse.move(x, y);
+    await page.mouse.down();
+    await page.mouse.move(x + 50, y + 50);
+    await page.mouse.up();
+
+    // 笔画只有画的端跟接收端会有细微的不同,刷新后笔迹才相同
+    await page.reload();
+    await page.waitForTimeout(2000);
+}
